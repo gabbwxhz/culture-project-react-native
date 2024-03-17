68d26a8c4340a7f494e0bf895a6c254ebe3241b6
gabriel <vnvsgabe@gmail.com>
2024-03-17 20:42:40 -0300

    instalado e configurado Eslint e Prettier

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View } from 'react-native'
 
-import Title from "./src/components/title/Title";
+import Title from './src/components/title/Title'
 
 export default function App() {
-  return <View style={styles.container}>
-    <Title></Title>
-  </View>;
+  return (
+    <View style={styles.container}>
+      <Title></Title>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
