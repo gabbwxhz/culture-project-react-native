38397a430c194b4428a14e73f718af5d396fee11
gabriel <vnvsgabe@gmail.com>
2024-03-17 22:22:17 -0300

    aplicado useState e aplicada a logica para funcionamento

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,21 +1,58 @@
 import React from 'react'
 import { View, Text, TextInput, Button } from 'react-native'
 
+import { useState } from 'react'
+
 import ResultIMC from './result-imc/ResultIMC'
 
 export default function Form() {
+  const [height, setHeight] = useState(null)
+  const [weight, setWeight] = useState(null)
+  const [messageIMC, setMessageIMC] = useState('Preencha o peso e altura')
+  const [resultIMC, setRestulIMC] = useState(null)
+  const [textButton, setTextButton] = useState('Calcular')
+
+  function calculateIMC() {
+    return setRestulIMC((weight / (height * height)).toFixed(2))
+  }
+
+  function validationIMC() {
+    if (weight !== null && height !== null) {
+      calculateIMC()
+      setHeight(null)
+      setWeight(null)
+      setMessageIMC('Seu IMC é igual a:')
+      setTextButton('Calcular novamente')
+      return
+    } else {
+      setRestulIMC(null)
+      setTextButton('Calcular')
+      setMessageIMC('Preencha o peso e altura')
+    }
+  }
+
   return (
     <View>
       <View>
         <Text>Altura</Text>
-        <TextInput placeholder="Ex. 1.75" keyboardType="numeric" />
+        <TextInput
+          onChangeText={setHeight}
+          value={height}
+          placeholder="Ex. 1.75"
+          keyboardType="numeric"
+        />
 
         <Text>Peso</Text>
-        <TextInput placeholder="Ex. 60.85" keyboardType="numeric" />
+        <TextInput
+          onChangeText={setWeight}
+          value={weight}
+          placeholder="Ex. 60.85"
+          keyboardType="numeric"
+        />
 
-        <Button title="Calcular IMC" />
+        <Button title={textButton} onPress={() => validationIMC()} />
       </View>
-      <ResultIMC messageResultIMC={messageResultIMC} resultIMC={resultIMC} />
+      <ResultIMC messageResultIMC={messageIMC} resultIMC={resultIMC} />
     </View>
   )
 }
