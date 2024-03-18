5b26ceebb74cde0b4d0f46ce62a9c8606200110e
gabriel <vnvsgabe@gmail.com>
2024-03-17 22:21:50 -0300

    criado props para ResultIMC

--- src/components/form/result-imc/ResultIMC.js
+++ src/components/form/result-imc/ResultIMC.js
@@ -4,8 +4,8 @@ import { View, Text } from 'react-native'
 export default function ResultIMC(props) {
   return (
     <View>
-      <Text>{props.resultIMC}</Text>
       <Text>{props.messageResultIMC}</Text>
+      <Text>{props.resultIMC}</Text>
     </View>
   )
 }
